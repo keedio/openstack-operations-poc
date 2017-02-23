1e00dc0accc4587e6017068097d8f2c81c60a1ec
David Santamaria <dsantamaria@keedio.com>
2017-02-23 15:12:20 +0100

    Borrado de filtros implementado

--- app/raw-logs/raw-logs.component.js
+++ app/raw-logs/raw-logs.component.js
@@ -56,17 +56,21 @@ app.component('rawLogs',{
             self.payLoad = "";
             self.dateFrom = "";
             self.dateTo = "";
-
             self.pageStates = [];
             self.actualIndex = 0;
-            self.getRawLogsBy = function  () {
+            self.activeFilters= {};
 
-                RawLogsService.GetRawLogsBy(serializeForm(), undefined).then(function (data) {
+            self.getRawLogsBy = function  () {
+                var filters = serializeForm();
+                RawLogsService.GetRawLogsBy(filters, undefined).then(function ( data) {
                     self.rawlogs =  data.result;
                     self.pageStates = [];
                     self.pageStates.push(undefined);
                     self.actualIndex = 1;
                     self.pageStates.push(data.pageState);
+                    
+                    self.activeFilters = getActiveFilters(filters);
+
                 });
             }
 
@@ -98,6 +102,37 @@ app.component('rawLogs',{
                 return loglevel === 'ERROR' ? "pficon pficon-error-circle-o" : loglevel === 'WARNING' ?  "pficon pficon-warning-triangle-o " : "pficon pficon-ok";
             }
 
+            self.removeFilter = function(filter ) {
+
+                switch(filter) {
+                    case 'loglevel':
+                        self.logLevel.selectedOption = undefined;
+                        $("#loglevelSelect").selectpicker('val', -1);
+                        break;
+                    case 'service':
+                        self.servicesEntries.selectedOption = undefined;
+                        $("#serviceSelect").selectpicker('val', -1);
+                        break;
+                    case 'region':
+                        self.regionEntries.selectedOption = undefined;
+                        $("#regionSelect").selectpicker('val', -1);
+                        break;
+                    case 'node_type':
+                        self.nodeType.selectedOption = undefined;
+                        $("#node_typeSelect").selectpicker('val', -1);
+                        break;
+                    case 'date':
+                        self.dateFrom = '';
+                        self.dateTo = '';
+                        break;
+                    case 'payload':
+                        self.payLoad = '';
+                        break;
+                }
+                self.getRawLogsBy();
+
+            }
+
             function splitSelectedOtions(data) {
 
                 var array = []
@@ -108,14 +143,22 @@ app.component('rawLogs',{
 
             function serializeForm(){
                 return  {
-                    node_type : self.nodeType.selectedOption != undefined ? splitSelectedOtions(self.nodeType.selectedOption) : undefined,
-                    loglevel : self.logLevel.selectedOption != undefined ? splitSelectedOtions(self.logLevel.selectedOption) : undefined,
-                    region : self.regionEntries.selectedOption != undefined ? splitSelectedOtions(self.regionEntries.selectedOption) : undefined,
-                    service : self.servicesEntries.selectedOption != undefined ? splitSelectedOtions(self.servicesEntries.selectedOption) : undefined,
+                    node_type : self.nodeType.selectedOption != undefined  && self.nodeType.selectedOption != '' ? splitSelectedOtions(self.nodeType.selectedOption) : undefined,
+                    loglevel : self.logLevel.selectedOption != undefined && self.logLevel.selectedOption != '' ? splitSelectedOtions(self.logLevel.selectedOption) : undefined,
+                    region : self.regionEntries.selectedOption != undefined && self.regionEntries.selectedOption != '' ? splitSelectedOtions(self.regionEntries.selectedOption) : undefined,
+                    service : self.servicesEntries.selectedOption != undefined && self.servicesEntries.selectedOption != ''? splitSelectedOtions(self.servicesEntries.selectedOption) : undefined,
                     payload : self.payLoad != '' ? self.payLoad : undefined,
                     date : self.dateFrom != '' &&  self.dateTo != '' ? [  self.dateFrom , self.dateTo ] : undefined
 
                 };
+            }
+
+            function getActiveFilters(filters){
+                var ret = [];
+                for (var filter in filters )
+                    if(filters[filter] != undefined && filters[filter] != '') ret.push({"name": filter, "values": filters[filter].toString()})
+
+                return ret;
             }
 
         }]
