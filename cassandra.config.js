7724325d119a4c84db6eac4b57dc67cd05f6fb2c
David Santamaría <dsantamaria@keedio.com>
2017-04-05 09:35:09 +0200

    Set cassandra contacpoint to cassandra service

--- cassandra.config.js
+++ cassandra.config.js
@@ -2,7 +2,7 @@
  * Created by davidsantamaria on 20/2/17.
  */
 var cassandra=require("cassandra-driver");
-var client =new cassandra.Client({'contactPoints':['10.129.135.122'],keyspace:'redhatpoc'});
+var client =new cassandra.Client({'contactPoints':[process.env.CASSANDRA_SERVICE],keyspace:'redhatpoc'});
 
 /*
  * process.env.CASSANDRA_SERVICE
